c0a9a6add438896fd74999d1f4abaa21e3c87e3b
mateusz-grodek <mateusz-grodek>
2024-04-04 19:08:49 +0000

    add access keys auth method

--- set-up/1.access_adfs_using_access_keys.py
+++ set-up/1.access_adfs_using_access_keys.py
@@ -1,2 +1,22 @@
 # Databricks notebook source
-print('hello')
+# MAGIC %md
+# MAGIC #### Access Azure Data Lake using access keys
+# MAGIC 1. Set the spark config fs.azure.key
+# MAGIC 1. List files from demo container
+# MAGIC 1. Read data from csv file
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
