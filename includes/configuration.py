de166c420acef9ff41f95050dd7836c191051d59
mateusz-grodek <mateusz-grodek>
2024-04-04 19:38:12 +0000

    Add include folder
    folder contain common function and declared variables

---
+++ includes/configuration.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+#Assign datasource path to variable with mount
+raw_folder_path = '/mnt/databricksdlmg/raw'
+processed_folder_path = '/mnt/databricksdlmg/processed'
+presentation_folder_path = '/mnt/databricksdlmg/presentation'
+
+# COMMAND ----------
+
+#Assign datasource path to variable with out mount
+#raw_folder_path = 'abfss://raw@formula1dl.dfs.core.windows.net'
+#processed_folder_path = 'abfss://processed@formula1dl.dfs.core.windows.net'
+#presentation_folder_path = 'abfss://presentation@formula1dl.dfs.core.windows.net'
