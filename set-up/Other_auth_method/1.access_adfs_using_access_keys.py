e0a8d9647c9191df490dc5ac570062251c0befaa
Mateusz Gródek <161132280+mateusz-grodek@users.noreply.github.com>
2024-07-04 14:08:31 +0200

    Update 1.access_adfs_using_access_keys.py
    delete unneceserry desc

--- set-up/Other_auth_method/1.access_adfs_using_access_keys.py
+++ set-up/Other_auth_method/1.access_adfs_using_access_keys.py
@@ -1,9 +1,6 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC #### Access Azure Data Lake using access keys
-# MAGIC 1. Set the spark config fs.azure.key
-# MAGIC 1. List files from demo container
-# MAGIC 1. Read data from csv file
 
 # COMMAND ----------
 
